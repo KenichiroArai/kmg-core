fa6ccdaefe01e1a98aa7be6c8f0e1cf1c3ab8290
KenichiroArai <sammyleejunior@gmail.com>
2024-03-22 23:20:16 +0900

    ＫＭＧ性能測定サービスを作成した。

---
+++ src/main/java/kmg/core/domain/service/KmgPfaMeasService.java
@@ -0,0 +1,30 @@
+package kmg.core.domain.service;
+
+/**
+ * ＫＭＧ性能測定サービスインタフェース<br>
+ *
+ * @author KenichiroArai
+ * @sine 1.0.0
+ * @version 1.0.0
+ */
+public interface KmgPfaMeasService {
+
+    /**
+     * 開始<br>
+     *
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     */
+    void start();
+
+    /**
+     * 終了<br>
+     *
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     */
+    void end();
+
+}
---
+++ src/main/java/kmg/core/domain/service/impl/KmgPfaMeasServiceImpl.java
@@ -0,0 +1,62 @@
+package kmg.core.domain.service.impl;
+
+import kmg.core.domain.model.KmgPfaMeasModel;
+import kmg.core.domain.service.KmgPfaMeasService;
+
+/**
+ * ＫＭＧ性能測定サービス<br>
+ *
+ * @author KenichiroArai
+ * @sine 1.0.0
+ * @version 1.0.0
+ */
+public class KmgPfaMeasServiceImpl implements KmgPfaMeasService {
+
+    /** 名称 */
+    private final String name;
+
+    /** ＫＭＧ性能測定モデル */
+    private final KmgPfaMeasModel kmgPfaMeasModel;
+
+    /**
+     * コンストラクタ<br>
+     *
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     * @param name
+     *                 名称
+     */
+    public KmgPfaMeasServiceImpl(final String name) {
+        this.name = name;
+        this.kmgPfaMeasModel = new KmgPfaMeasModel();
+    }
+
+    /**
+     * 開始<br>
+     *
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     */
+    @Override
+    public void start() {
+        System.out.println(String.format("%s：開始", this.name));   //$NON-NLS-1$
+        this.kmgPfaMeasModel.start();
+    }
+
+    /**
+     * 終了<br>
+     *
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     */
+    @Override
+    public void end() {
+        this.kmgPfaMeasModel.end();
+        System.out.println(String.format("%s：終了。経過時間=[%f%s]", this.name, this.kmgPfaMeasModel.getElapsedTime(), //$NON-NLS-1$
+                this.kmgPfaMeasModel.getTimeUnit().getUnitName()));
+    }
+
+}
