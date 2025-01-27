7146de82ce9e7d58170752ceff54bcf3c030d6c1
KenichiroArai <sammyleejunior@gmail.com>
2025-01-27 20:20:28 +0900

    KmgCoreApplicationクラスから不要なロガーとコンストラクタを削除し、mainメソッドのリソース管理を改善

--- src/main/java/kmg/core/KmgCoreApplication.java
+++ src/main/java/kmg/core/KmgCoreApplication.java
@@ -1,7 +1,5 @@
 package kmg.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -16,25 +14,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class KmgCoreApplication {
 
-    /** ロガー */
-    private static final Logger LOGGER = LoggerFactory.getLogger(KmgCoreApplication.class);
-
-    /** 構成可能なアプリケーションコンテキスト */
-    private final ConfigurableApplicationContext context;
-
-    /**
-     * コンストラクタ<br>
-     *
-     * @author KenichiroArai
-     * @sine 1.0.0
-     * @version 1.0.0
-     * @param context
-     *                構成可能なアプリケーションコンテキスト
-     */
-    public KmgCoreApplication(final ConfigurableApplicationContext context) {
-        this.context = context;
-    }
-
     /**
      * エントリポイント<br>
      *
@@ -45,11 +24,11 @@ public class KmgCoreApplication {
      *             オプション
      */
     public static void main(final String[] args) {
-        try (ConfigurableApplicationContext context = SpringApplication.run(KmgCoreApplication.class, args);) {
-            @SuppressWarnings("unused")
-            final KmgCoreApplication kmgCoreApplication = context.getBean(KmgCoreApplication.class);
-            // 処理無し
-        }
+
+        @SuppressWarnings("resource")
+        final ConfigurableApplicationContext ctx = SpringApplication.run(KmgCoreApplication.class, args);
+        ctx.close();
+
     }
 
 }
