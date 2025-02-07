ad6dc2cb167c1337f1d622a376f64188ae14e169
KenichiroArai <sammyleejunior@gmail.com>
2025-02-07 20:45:02 +0900

    KmgExceptionクラスにメッセージの種類と引数を追加し、KmgMessageUtilsを使用してメッセージを取得するように変更しました。また、コンストラクタを整理しました。

--- src/main/java/kmg/core/infrastructure/exception/KmgException.java
+++ src/main/java/kmg/core/infrastructure/exception/KmgException.java
@@ -1,6 +1,7 @@
 package kmg.core.infrastructure.exception;
 
-import kmg.core.infrastructure.model.KmgMessageModel;
+import kmg.core.infrastructure.types.KmgMsgMessageTypes;
+import kmg.core.infrastructure.utils.KmgMessageUtils;
 
 /**
  * KMG例外<br>
@@ -14,8 +15,14 @@ public class KmgException extends Exception {
     /** デフォルトシリアルバージョンUID */
     private static final long serialVersionUID = 1L;
 
-    /** メッセージモデル */
-    private final KmgMessageModel kmgMessageModel;
+    /** メッセージメッセージの種類 */
+    private KmgMsgMessageTypes messageTypes;
+
+    /** メッセージメッセージの引数 */
+    private Object[] messageArgs;
+
+    /** メッセージ */
+    private final String message;
 
     /**
      * コンストラクタ<br>
@@ -23,12 +30,12 @@ public class KmgException extends Exception {
      * @author KenichiroArai
      * @sine 1.0.0
      * @version 1.0.0
-     * @param kmgMessageModel
-     *                    メッセージモデル
+     * @param messageTypes
+     *                     メッセージの種類
      */
-    public KmgException(final KmgMessageModel kmgMessageModel) {
+    public KmgException(final KmgMsgMessageTypes messageTypes) {
 
-        this(kmgMessageModel, null);
+        this.message = KmgMessageUtils.getMessage(messageTypes, null);
 
     }
 
@@ -38,15 +45,52 @@ public class KmgException extends Exception {
      * @author KenichiroArai
      * @sine 1.0.0
      * @version 1.0.0
-     * @param kmgMessageModel
-     *                    メッセージモデル
-     * @param cause
-     *                    原因
+     * @param messageTypes
+     *                     メッセージの種類
+     * @param messageArgs
+     *                     メッセージの引数
      */
-    public KmgException(final KmgMessageModel kmgMessageModel, final Throwable cause) {
+    public KmgException(final KmgMsgMessageTypes messageTypes, final Object[] messageArgs) {
+
+        this.message = KmgMessageUtils.getMessage(messageTypes, messageArgs);
+
+    }
+
+    /**
+     * コンストラクタ<br>
+     *
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     * @param messageTypes
+     *                     メッセージの種類
+     * @param cause
+     *                     原因
+     */
+    public KmgException(final KmgMsgMessageTypes messageTypes, final Throwable cause) {
 
         super(cause);
-        this.kmgMessageModel = kmgMessageModel;
+        this.message = KmgMessageUtils.getMessage(messageTypes, null);
+
+    }
+
+    /**
+     * コンストラクタ<br>
+     *
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     * @param messageTypes
+     *                     メッセージの種類
+     * @param messageArgs
+     *                     メッセージの引数
+     * @param cause
+     *                     原因
+     */
+    public KmgException(final KmgMsgMessageTypes messageTypes, final Object[] messageArgs, final Throwable cause) {
+
+        super(cause);
+        this.message = KmgMessageUtils.getMessage(messageTypes, messageArgs);
 
     }
 
@@ -61,22 +105,38 @@ public class KmgException extends Exception {
     @Override
     public String getMessage() {
 
-        final String result = this.kmgMessageModel.getMessage();
+        final String result = this.message;
         return result;
 
     }
 
     /**
-     * メッセージモデルを返す。
+     * メッセージの引数を返す。<br>
      *
-     * @return メッセージモデル
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     * @return メッセージの引数
      */
-    public KmgMessageModel getMessageInfo() {
+    public Object[] getMessageArgs() {
 
-        final KmgMessageModel result = this.kmgMessageModel;
-
+        final Object[] result = this.messageArgs;
         return result;
 
     }
 
+    /**
+     * メッセージの種類を返す。<br>
+     *
+     * @author KenichiroArai
+     * @sine 1.0.0
+     * @version 1.0.0
+     * @return メッセージの種類
+     */
+    public KmgMsgMessageTypes getMessageTypes() {
+
+        final KmgMsgMessageTypes result = this.messageTypes;
+        return result;
+
+    }
 }
