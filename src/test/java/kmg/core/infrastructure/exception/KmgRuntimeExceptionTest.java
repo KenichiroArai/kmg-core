f9e24a0e93e2f3bcdfa22f18c17b41a330c9f930
KenichiroArai <sammyleejunior@gmail.com>
2025-05-05 01:03:43 +0900

    KmgRuntimeExceptionクラスを新規作成し、例外メッセージの生成機能を実装。KmgSystemExceptionクラスを削除。

--- src/test/java/kmg/core/infrastructure/exception/KmgRuntimeExceptionTest.java
+++ src/test/java/kmg/core/infrastructure/exception/KmgRuntimeExceptionTest.java
@@ -9,11 +9,11 @@ import kmg.core.domain.types.KmgCoreGenMessageTypes;
 import kmg.core.infrastructure.common.KmgComExcMessageTypes;
 
 /**
- * KMGシステム例外テスト<br>
+ * KMG実行時例外テスト<br>
  *
  * @author KenichiroArai
  *
- * @since 0.1.0
+ * @since 0.2.0
  *
  * @version 0.2.0
  */
@@ -21,14 +21,14 @@ import kmg.core.infrastructure.common.KmgComExcMessageTypes;
     "nls", "static-method"
 })
 @ExtendWith(MockitoExtension.class)
-public class KmgSystemExceptionTest {
+public class KmgRuntimeExceptionTest {
 
     /**
      * デフォルトコンストラクタ<br>
      *
-     * @since 0.1.0
+     * @since 0.2.0
      */
-    public KmgSystemExceptionTest() {
+    public KmgRuntimeExceptionTest() {
 
         // 処理なし
     }
@@ -36,7 +36,7 @@ public class KmgSystemExceptionTest {
     /**
      * コンストラクタ メソッドのテスト - 正常系：メッセージタイプのみを指定した場合
      *
-     * @since 0.1.0
+     * @since 0.2.0
      */
     @Test
     public void testConstructor_normalMessageTypes() {
@@ -46,7 +46,7 @@ public class KmgSystemExceptionTest {
         final String                 expectedMessage  = "{0}がありません。";
 
         /* テスト対象の実行 */
-        final KmgSystemException testException = new KmgSystemException(expectedMsgTypes);
+        final KmgRuntimeException testException = new KmgRuntimeException(expectedMsgTypes);
 
         /* 検証の準備 */
         final String                actualMessage  = testException.getMessage();
@@ -61,7 +61,7 @@ public class KmgSystemExceptionTest {
     /**
      * コンストラクタ メソッドのテスト - 正常系：メッセージタイプとメッセージ引数を指定した場合
      *
-     * @since 0.1.0
+     * @since 0.2.0
      */
     @Test
     public void testConstructor_normalMessageTypesAndArgs() {
@@ -74,7 +74,7 @@ public class KmgSystemExceptionTest {
         final String                 expectedMessage  = "テスト引数1がありません。";
 
         /* テスト対象の実行 */
-        final KmgSystemException testException = new KmgSystemException(expectedMsgTypes, expectedMsgArgs);
+        final KmgRuntimeException testException = new KmgRuntimeException(expectedMsgTypes, expectedMsgArgs);
 
         /* 検証の準備 */
         final String                actualMessage  = testException.getMessage();
@@ -91,7 +91,7 @@ public class KmgSystemExceptionTest {
     /**
      * コンストラクタ メソッドのテスト - 正常系：メッセージタイプと原因を指定した場合
      *
-     * @since 0.1.0
+     * @since 0.2.0
      */
     @Test
     public void testConstructor_normalMessageTypesAndCause() {
@@ -102,7 +102,7 @@ public class KmgSystemExceptionTest {
         final Throwable              expectedCause    = new RuntimeException("テスト原因");
 
         /* テスト対象の実行 */
-        final KmgSystemException testException = new KmgSystemException(expectedMsgTypes, expectedCause);
+        final KmgRuntimeException testException = new KmgRuntimeException(expectedMsgTypes, expectedCause);
 
         /* 検証の準備 */
         final String                actualMessage  = testException.getMessage();
