3b47f95b4c26fc88e2fae964b5958e71c775e988
KenichiroArai <sammyleejunior@gmail.com>
2025-09-04 18:51:33 +0900

    #79 コードのJavadocの整備。
    (kmg.core.infrastructure.modelのJavadocの整備 #79)

--- src/test/java/kmg/core/infrastructure/model/impl/KmgReflectionModelImplTest.java
+++ src/test/java/kmg/core/infrastructure/model/impl/KmgReflectionModelImplTest.java
@@ -43,8 +43,6 @@ public class KmgReflectionModelImplTest extends AbstractKmgTest {
      * @author KenichiroArai
      *
      * @since 0.1.0
-     *
-     * @version 0.2.0
      */
     private static class TestClass {
 
